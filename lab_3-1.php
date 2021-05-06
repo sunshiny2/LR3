ceb6fd6fa28267a4da3fd371cceff74302c5e97b
sunshiny2 <iarulina.sasha@mail.ru>
2021-05-06 21:31:13 +0500

    LR3_Yarulina

---
+++ lab_3-1.php
@@ -0,0 +1,16 @@
+﻿<?php
+$tek=$_GET["tek"];
+$jsonString = file_get_contents("lab_3-1.json");
+$rrow = json_decode($jsonString);
+$counter=0;
+foreach ($rrow->new as $number => $item) {
+    $counter++;
+    $tit = $item->tit;
+    $text = $item->text;
+    $img = $item->img;
+    echo "<div style='width: 400px; background-color: #fee2ee;' >";
+    echo '<h3>'.$tit.'</h3><p>'.$text.'</p><img src="'.$img.'" width=400px><br>';  
+    echo "</div><br>";
+    if($counter==$tek)break;
+}
+?>
---
+++ lab_3-2.php
@@ -0,0 +1,27 @@
+﻿<?php
+        $jsonString = file_get_contents("lab_3-2.json", JSON_UNESCAPED_UNICODE);
+        $rrow = json_decode($jsonString);
+        $counter=0;
+        $count = count($rrow->review);
+        foreach ($rrow->review as $number => $item) {
+            $counter++;
+            $name = $item->name;
+            $email = $item->email;
+            $text = $item->text;
+            ?>
+<html>
+  <style>
+    .container {   width: 75%;
+              border: 2px solid #ccc;
+              background-color: #eee;
+              border-radius: 5px; /* скругление углов блока */
+              padding: 16px; /* внутренние поля */
+            };
+  </style>
+             <div class='container' >
+            <?
+            echo '<span style="font-weight: bold;">Имя: </span>'.$name.'<br><span style="font-weight: bold;">Почта: </span>'.$email.'<br><span style="font-weight: bold;">Отзыв: </span>'.$text.'<br>';  
+            echo "</div><br>";
+            if($counter==$count)break;
+        }
+?>
